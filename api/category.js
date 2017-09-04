e2a21838876b41addd88c5e2a38d9b991f60313f
tianzhen <340349237@qq.com>
2017-09-04 21:42:08 +0800

    修复id生成函数

--- api/category.js
+++ api/category.js
@@ -10,13 +10,16 @@ const randNumLib = require('../lib/rand-num.fn');
 
 router.route('/category')
   .get((req, res, next) => {
-    next();
+    const data = db.get('categories').value();
+    res.status(200).json({
+      status: 1,
+      message: lang.OK,
+      data: data
+    });
   })
   .post((req, res, next) => {
     const name = req.body.name;
-    const rand = randNumLib.getRandNum(6);
-    const date = (new Date().getTime()).toString(16);
-    const id = date + rand;
+    const id = randNumLib.idBuild();
     db.get('categories').push({
       id: id,
       name: name
